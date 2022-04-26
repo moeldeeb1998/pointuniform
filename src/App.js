b94995a1ad70218f50c3fa47737ed3ee20387930
Mohamed A. Eldeeb <meldeeb1998@gmail.com>
2022-04-26 03:26:22 +0200

    create maintenance page

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Maintenance from "./components/pages/misc/Maintenance";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./styles/main.scss";
+
+const App = () => {
+  return (
+    <div className="App">
+      <Maintenance />
+    </div>
+  );
+};
+
+export default App;
