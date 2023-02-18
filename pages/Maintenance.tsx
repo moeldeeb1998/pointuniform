493152f94fe9a93ecc626ac6cccf24e606ba3ebd
Mohamed Eldeeb <meldeeb1998@gmail.com>
2023-02-18 14:40:16 +0200

    restruct the react app to next app

---
+++ pages/Maintenance.tsx
@@ -0,0 +1,33 @@
+import "../styles/pages/misc.module.scss";
+
+const Maintenance = () => {
+  return (
+    <div className="misc-wrapper">
+      <a className="brand-logo" href="/">
+        <img width={300} src="/assets/logo.png" alt="POINT" />
+      </a>
+      <div className="misc-inner p-2 p-sm-3">
+        <div className="w-100 text-center">
+          <h2 className="mb-1"> 🛠 تحت الصيانة</h2>
+          <img
+            width={100}
+            src="/assets/under-maintenance.png"
+            alt="صفحة تحت الصيانة"
+          />
+          <p className="mb-3">
+            نأسف للإزعاج ولكننا نجري بعض الصيانة في الوقت الحالي
+          </p>
+          <div>
+            <a
+              className="btn btn-success"
+              href="mailto: mahmoud.gad@pointuniform.com"
+            >
+              تواصل معنا
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Maintenance;
