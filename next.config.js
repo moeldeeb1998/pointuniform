10202729c6c30cec520fb6166e49b33e4598f4b8
Mohamed Eldeeb <meldeeb1998@gmail.com>
2024-11-03 00:27:17 +0200

    configure github pages for next app not react

--- next.config.js
+++ next.config.js
@@ -9,3 +9,22 @@ const nextConfig = {
 };
 
 module.exports = nextConfig;
+
+// next.config.js
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+let assetPrefix = "";
+let basePath = "";
+
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
+  assetPrefix = `/${repo}/`;
+  basePath = `/${repo}`;
+}
+
+module.exports = {
+  assetPrefix,
+  basePath,
+  images: {
+    unoptimized: true,
+  },
+};
--- src/components/Maintenance.tsx
+++ src/components/Maintenance.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Image from "next/image";
 import Link from "next/link";
 // import "../styles/pages/misc.module.scss";
 
@@ -9,12 +10,12 @@ const Maintenance = () => {
   return (
     <div className="misc-wrapper">
       <Link className="brand-logo" href="/">
-        <img width={300} src="/assets/logo.png" alt="POINT" />
+        <Image width={300} src="/assets/logo.png" alt="POINT" />
       </Link>
       <div className="misc-inner p-2 p-sm-3">
         <div className="w-100 text-center">
           <h2 className="mb-1"> 🛠 تحت الصيانة</h2>
-          <img
+          <Image
             width={100}
             src="/assets/under-maintenance.png"
             alt="صفحة تحت الصيانة"
